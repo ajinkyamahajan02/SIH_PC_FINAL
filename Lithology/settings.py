fe193d01c71d12dc58bd512b7a4de641e1f64cf3
ajinkyamahajan02 <ajinkyamahajan1896@gmail.com>
2023-12-20 18:05:05 +0530

    v3

--- Lithology/settings.py
+++ Lithology/settings.py
@@ -122,7 +122,6 @@ STATICFILES_DIRS=[
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'temp_images/input') # media directory in the root directory
 MEDIA_URL = 'temp_images/input/'
-STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
